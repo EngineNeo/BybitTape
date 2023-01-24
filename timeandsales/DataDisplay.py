fe8fc607571fc4d69434b44236d4f3bd27919baa
EngineNeo <neomaralit@gmail.com>
2023-01-23 23:46:51 -0500

    Added colors and filter function

--- timeandsales/DataDisplay.py
+++ timeandsales/DataDisplay.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 class DataDisplay:
     def __init__(self, current_symbols):
 
@@ -12,13 +11,26 @@ class DataDisplay:
 
         # Dropdown menu/combobox for symbols
         self.symbol_selector = tk.StringVar()
-        self.symbol_combobox = ttk.Combobox(self.root, width = 27, textvariable = self.symbol_selector, state="readonly")
+        self.symbol_combobox = ttk.Combobox(self.root, width = 27, textvariable = self.symbol_selector, state="normal")
         self.symbol_combobox['values'] = (current_symbols)
-        self.symbol_combobox.set("ETHUSD") #set default value to ETHUSD
+        self.symbol_combobox.set("ETHUSD") # Default value of ticker to track
         self.symbol_combobox.grid(sticky='W', row = 0, column = 0)
-
+        
         self.selected_symbol = self.symbol_selector.get()
         
+        # Adding "Filter" label
+        filter_label = tk.Label(self.root, text="Filter", font=("Arial", 10))
+        filter_label.grid(row=0, column=0, padx=10, sticky='W')
+
+        # Adding input field for filtering by quantity
+        self.filter_entry = tk.Entry(self.root, width=5)
+        self.filter_entry.grid(row=0, column=1, padx=10, sticky='W')
+
+        # Adding a button to apply the filter
+        self.apply_filter_button = tk.Button(self.root, text="Apply Filter")
+        self.apply_filter_button.grid(row=0, column=2, padx=10, sticky='W')
+
+
         # Creating column headers
         self.tree = ttk.Treeview(self.root, columns=("Ticker", "Side", "Price", "Quantity", "Timestamp"))
         self.tree.heading("#0", text="", anchor=tk.W)
@@ -44,7 +56,7 @@ class DataDisplay:
         self.tree.configure(yscrollcommand=v_scroll.set)
 
         # Pack the tree widget
-        self.tree.grid(row=1, column=0, columnspan=2, rowspan=99,sticky='nsew') 
+        self.tree.grid(row=1, column=0, rowspan=99,sticky='nsew') 
 
         # Add a row and column configuration to the grid layout manager
         self.root.grid_rowconfigure(1, weight=1)
@@ -52,9 +64,13 @@ class DataDisplay:
         
     # Inserting data into columns
     def insert_data(self, ticker, side, price, quantity, timestamp):
-        self.tree.tag_configure("Buy", background="blue", foreground = "white")
-        self.tree.tag_configure("Sell", background="red", foreground = "white")
-        item_id = self.tree.insert("", "end", values=(ticker, side, price, quantity, timestamp), tags = side.lower())
+    # Add tags to the side column
+        if side == "Sell":
+            self.tree.tag_configure("sell", background="red", foreground="white")
+            item_id = self.tree.insert("", "end", values=(ticker, side, price, quantity, timestamp), tags = "sell")
+        elif side == "Buy":
+            self.tree.tag_configure("buy", background="blue", foreground="white")
+            item_id = self.tree.insert("", "end", values=(ticker, side, price, quantity, timestamp), tags = "buy")
         self.tree.see(item_id)
 
     # Method to clear the treeview
--- timeandsales/timeandsales.py
+++ timeandsales/timeandsales.py
@@ -10,8 +10,22 @@ def main():
         display.clear_tree()
         selected_symbol = display.symbol_combobox.get()
         trade_data.display_trades(selected_symbol, display)
+        
+    def apply_filter():
+            # Get the filter value from the input field
+            filter_value = display.filter_entry.get()
+            if filter_value:
+                # Convert the value to an int and use it to filter the data displayed in the treeview
+                filter_value = int(filter_value)
+                for item in display.tree.get_children():
+                    quantity = display.tree.item(item, "values")[3]
+                    if quantity < filter_value:
+                        display.tree.delete(item)
 
+    # Combobox events
     display.symbol_combobox.bind("<<ComboboxSelected>>", on_select)
+    display.filter_entry.bind("<Return>", apply_filter)
+    
     selected_symbol = display.symbol_combobox.get()
     trade_data.display_trades(selected_symbol, display)
     display.start()
