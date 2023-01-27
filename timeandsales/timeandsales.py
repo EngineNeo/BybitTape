bd0bb058855a9ef320ef1de3723ae5615bc64bb8
unknown <neomaralit@gmail.com>
2023-01-27 11:16:57 -0500

    Added ignore file for pycache

--- timeandsales/timeandsales.py
+++ timeandsales/timeandsales.py
@@ -4,35 +4,38 @@ from DataDisplay import DataDisplay
 def main():
     trade_data = TradeData()
     allowed_symbols = trade_data.open_symbols
-    display = DataDisplay(allowed_symbols) # Create an instance of DataDisplay
+    display = DataDisplay(allowed_symbols)
     filter_value = display.filter_quantity.get()
-    selected_symbol = display.symbol_combobox.get()
 
-    def on_select():
+    def on_select(event):
         display.clear_tree()
-        trade_data.display_trades(selected_symbol, display, filter_value)
-        
-    def filter_trades():
-        # Get value from user input
-        display.clear_tree() # Clear tree before adding new data
+        filter_value = display.filter_quantity.get()
+        selected_symbol = display.symbol_combobox.get()
         trade_data.display_trades(selected_symbol, display, filter_value)
 
-    def validate_int(P): # Function for the following int validation
-        if P.isdigit():
+    def validate_int_backspace_return(P, d):
+        if d == '1':  # key pressed is backspace
             return True
-        return False
+        elif d == '0':  # key pressed is return
+            return True
+        elif P.isdigit():  # input is an int
+            return True
+        else:
+            return False
 
+
+    display.filter_entry.insert(0, '1')
     # Only allow integers to be entered
     display.filter_entry.config(validate="key")
-    display.filter_entry.config(validatecommand=(display.root.register(validate_int), '%P'))
+    display.filter_entry.config(validatecommand=(display.root.register(validate_int_backspace_return), '%P', '%d'))
 
     # Combobox event
     display.symbol_combobox.bind("<<ComboboxSelected>>", on_select)
 
     # Filter Event
-    display.filter_entry.bind("<Return>", filter_trades)
+    display.filter_entry.bind("<Return>", on_select)
 
-    selected_symbol = display.symbol_combobox.get() # Retrive Ticker from combobox
+    selected_symbol = display.symbol_combobox.get()
     trade_data.display_trades(selected_symbol, display, filter_value)
     display.start()
 
